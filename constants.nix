6015e590ceeb5f68eb166d10d57d41dc52b1e57c
Ben Duggan <benaduggan@gmail.com>
2024-08-03 15:21:52 -0400

    remove unneeded entries

--- constants.nix
+++ constants.nix
@@ -17,17 +17,13 @@ in
   digdugdevKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMBXiUz//dlW7zcblPxQZqgcmZ5KziuhDnnIbuFmOvOw";
 
   cacheSubstituters = [
-    "https://cache.nixos.org/"
     "https://benaduggan.cachix.org"
-    "https://devenv.cachix.org"
     "https://jacobi.cachix.org"
     "https://kwbauson.cachix.org"
   ];
 
   trustedPublicKeys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "benaduggan.cachix.org-1:BY2tmi++VqJD6My4kB/dXGfxT7nJqrOtRVNn9UhgrHE="
-    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     "jacobi.cachix.org-1:JJghCz+ZD2hc9BHO94myjCzf4wS3DeBLKHOz3jCukMU="
     "kwbauson.cachix.org-1:a6RuFyeJKSShV8LAUw3Jx8z48luiCU755DkweAAkwX0="
   ];
