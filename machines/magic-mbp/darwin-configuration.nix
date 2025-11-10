a9b55b015d998530089aac364c6be71b08eda3a4
Ben Duggan <benaduggan@gmail.com>
2025-11-10 11:37:26 -0700

    add new qwen 3 vl to my mac

--- machines/magic-mbp/darwin-configuration.nix
+++ machines/magic-mbp/darwin-configuration.nix
@@ -145,13 +145,13 @@
         #   ngl = 99;
         #   extraFlags = "-c 16384 --temp 0.6 --top-k 20 --min-p 0 --top-p 0.95";
         # };
-        qwen-3-coder-30-a3b-ud = {
+        qwen-3-vl-30b-ud = {
           inherit package;
           enable = true;
           port = 8017;
-          model = "/opt/box/models/qwen-3-coder-30b-a3b.gguf";
+          model = "/opt/box/models/Qwen3-VL-30B-A3B-Instruct-UD-Q4_K_XL.gguf";
           ngl = 99;
-          extraFlags = "-c 16384 --temp 0.6 --top-k 20 --min-p 0 --top-p 0.95";
+          extraFlags = "--mmproj /opt/box/models/mmproj-F16.gguf --n-gpu-layers 99 --jinja --top-p 0.8 --top-k 20 --temp 0.7 --min-p 0.0 --flash-attn on --presence-penalty 1.5 --ctx-size 16384";
         };
       };
     };
