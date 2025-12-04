bef6556946e23941e13c8595552b7de2a2af4530
Ben Duggan <benaduggan@gmail.com>
2025-12-04 16:34:42 -0700

    guh

--- home/bash.nix
+++ home/bash.nix
@@ -45,7 +45,7 @@ in
       export LANG=en_US.UTF-8
 
       # add local scripts to path
-      export PATH="$PATH:$HOME/.bin/:$HOME/.local/bin:~/.rd/bin"
+      export PATH="$PATH:$HOME/.bin/:$HOME/.local/bin:$HOME/.rd/bin"
       # mkdir -p $HOME/.completions
       # source $HOME/.completions/*
 
@@ -73,7 +73,7 @@ in
         fi
         git add -A
         git commit -nm "$MSG"
-        git put
+        git put --no-verify
       }
     '';
   };
