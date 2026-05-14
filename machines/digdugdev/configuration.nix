86a23493f779e472991c19f6c984b997cd49d306
Ben Duggan <benaduggan@gmail.com>
2026-05-14 11:37:40 -0400

    guh

--- machines/digdugdev/configuration.nix
+++ machines/digdugdev/configuration.nix
@@ -175,7 +175,7 @@ in
             crypto key sign-verify {env.JWT_SHARED_KEY}
             enable identity provider google
             cookie domain digdug.dev
-            trust login redirect uri domain digdug.dev
+            trust login redirect uri domain suffix digdug.dev path prefix /
             ui {
               links {
                 "My Identity" "/whoami" icon "las la-user"
